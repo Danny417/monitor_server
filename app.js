7e943c0c685ba15a7d6bb102e08e5c417f2e6e45
Danny Hsieh <danny.hsieh@outlook.com>
2015-02-09 21:26:12 -0800

    Not Completed

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var express = require('express'),
+    bodyParser = require('body-parser'),
+    sanitizer = require('sanitizer'),
+    config = require('./config.js'),
     app = express(),
     path = require('path'),
-	http = require('http');
-var server = http.Server(app);
-var io = require('socket.io')(server);
-var bl = require('bl');
-var dgram = require('dgram');
-var udpServer = dgram.createSocket('udp4');
-var nodes = {	
-};
+	  http = require('http'),
+    server = http.Server(app),
+    io = require('socket.io')(server),
+    bl = require('bl'),
+    dgram = require('dgram'),
+    udpServer = dgram.createSocket('udp4'),
+    nodes = {
+    };
 
 app.set('view_engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -16,83 +19,96 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res){
-	res.render('index.ejs', {"nodes" :nodes});		
+  'use strict';
+	res.render('index.ejs', {"nodes" :nodes});
 });
 
 app.get('/team', function(req, res){
+  'use strict';
   res.render('team.ejs');
 });
 
-app.post('/stopAll', function(req, res) {
-	for(var key in nodes) {
-		var buf = new Buffer("exit service");
-		udpServer.send(buf, 0, buf.length, 7777, key);
-	}
-	console.log('stop signal sent');
-	res.send("complete");
+app.post('/command', function(req, res) {
+  'use strict';
+  var key, buf,
+      santizingResult = JSON.parse(sanitizer.sanitize(JSON.stringify(req.body)));
+  for(key in nodes) {
+    if (nodes.hasOwnProperty(key)) {
+      buf = new Buffer(santizingResult.cmd);
+      udpServer.send(buf, 0, buf.length, config.UDPPORT_SEND, key);
+    }
+  }
+  res.send("complete");
 });
 
-app.post('/startAll', function(req, res) {
-	for(var key in nodes) {
-		var buf = new Buffer("start monitor");
-		udpServer.send(buf, 0, buf.length, 7777, key);
-	}
-	console.log('start signal sent');
-	res.send("complete");
+app.post('/update', function(req, res) {
+  'use strict';
+  var buf = new Buffer('active');
+  udpServer.send(buf, 0, buf.length, config.UDPPORT_SEND, '127.0.0.1');
+  res.send("complete");
 });
 
-app.post('/pauseAll', function(req, res) {
-	for(var key in nodes) {
-		var buf = new Buffer("stop monitor");
-		udpServer.send(buf, 0, buf.length, 7777, key);
-	}
-	console.log('pause signal sent');
-	res.send("complete");
-});
-server.listen((process.env.PORT || 80), function(){
-  console.log('listening on *:'+process.env.PORT);
+server.listen((process.env.PORT || 80), function() {
+  'use strict';
+  console.log('listening on *:'+( process.env.PORT || 80));
 });
 
 udpServer.on('listening', function () {
-    console.log('UDP Server listening on ' + udpServer.address().address + ":" + udpServer.address().port);
+  'use strict';
+  console.log('UDP Server listening on ' + udpServer.address().address + ":" + udpServer.address().port);
 });
 
-udpServer.on('message', function (msg, remote) {
-    console.log('from : ' + remote.address + ':' + remote.port);
-	var node = JSON.parse(msg.toString('utf8', 16));
+udpServer.on('message', function (data, remote) {
+  'use strict';
+  console.log('from : ' + remote.address + ':' + remote.port);
+  console.log(data.toString('utf8', 16));
+/*  var msg = JSON.parse(data.toString('utf8', 16));
+  if(!msg['error']) {
+
+  } else {
+
+  }
+  console.log(msg);*/
+	/*var node = JSON.parse(msg.toString('utf8', 16));
 	node.port = msg.readUInt16LE(4);
 	node.time = parseInt(msg.readUInt32LE(12).toString(16) + msg.readUInt32LE(8).toString(16), 16);
 	node.status = "online";
 	node.ip = intToIP(msg.readUInt32LE(0));
 	nodes[node.ip] = node;
 	console.log(node.ip + " : " + node.status);
-	io.emit('nodenamechange', node);
+	io.emit('nodenamechange', node);*/
 });
 
-udpServer.bind(5700);
+udpServer.bind(config.UDPPORT_RECEIVE);
 
 io.on('connection', function (socket) {
+  'use strict';
 	console.log('A user connected.');
 	socket.on('disconnect', function () {
 		console.log('A user disconnected.');
 	});
 });
-	
+
+/*
 setInterval(function() {
+  'use strict';
+  var key;
 	console.log("check nodes");
-	for(var key in nodes) {
+	for(key in nodes) {
 		if(nodes[key].time + 3 * 60 * 1000 < (new Date()).getTime() && nodes[key].status !== "offline") {
 			nodes[key].status = "offline";
 			io.emit('nodenamechange', nodes[key]);
 		}
 	}
 }, 3 * 60 * 1000);
+*/
 
-
+/*
 function intToIP(int) {
-    var part1 = int & 255;
-    var part2 = ((int >> 8) & 255);
-    var part3 = ((int >> 16) & 255);
-    var part4 = ((int >> 24) & 255);
-    return part1 + "." + part2 + "." + part3 + "." + part4;
-}
+  'use strict';
+  var part1 = int & 255,
+      part2 = ((int >> 8) & 255),
+      part3 = ((int >> 16) & 255),
+      part4 = ((int >> 24) & 255);
+  return part1 + "." + part2 + "." + part3 + "." + part4;
+}*/
---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+	UDPPORT_RECEIVE: 41171,
+	UDPPORT_SEND: 41170
+};
+
+module.exports = config;
