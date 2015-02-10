cc1f5a1edce9bcad0d9b045672a667fbe321cddb
Danny Hsieh <danny.hsieh@outlook.com>
2015-02-10 00:39:09 -0800

    update

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.set('view_engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({extended: false})); //to get handle on res.body
+app.use(bodyParser.json({extended: false}));
 
 app.get('/', function(req, res){
   'use strict';
@@ -31,13 +33,12 @@ app.get('/team', function(req, res){
 
 app.post('/command', function(req, res) {
   'use strict';
-  var key, buf,
-      santizingResult = JSON.parse(sanitizer.sanitize(JSON.stringify(req.body)));
+  var key, buf;
   for(key in nodes) {
     if (nodes.hasOwnProperty(key)) {
       if(!!nodes[key].ip) {
-        buf = new Buffer(santizingResult.cmd);
-        udpServer.send(buf, 0, buf.length, config.UDPPORT_SEND, nodes[key].ip);
+        buf = new Buffer(req.body.cmd);
+        udpServer.send(buf, 0, buf.length, config.UDPPORT_SEND, key);
       }
     }
   }
@@ -54,6 +55,13 @@ app.post('/update', function(req, res) {
 server.listen((process.env.PORT || 80), function() {
   'use strict';
   console.log('listening on *:'+( process.env.PORT || 80));
+  for(var i = 0; i < nodelist.length; i++) {
+    nodes[nodelist[i]] = {
+      'hostname' : nodelist[i],
+      'ping' : 'false',
+      'ip' : (nodelist[i] == "127.0.0.1") ? nodelist[i] : null
+      };
+  }
 });
 
 udpServer.on('listening', function () {
@@ -67,6 +75,8 @@ udpServer.on('message', function (data, remote) {
   console.log(data.toString('utf8', 16));
   var nodes = JSON.parse(data.toString('utf8', 16));
   io.emit('nodenamechange', nodes);
+  var buf = new Buffer('active');
+  udpServer.send(buf, 0, buf.length, config.UDPPORT_SEND, '127.0.0.1');
 /*  var msg = JSON.parse(data.toString('utf8', 16));
   if(!msg['error']) {
 
